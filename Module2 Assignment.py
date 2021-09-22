5bab2cb35d80453131768e8a05ef6e6dd109c06e
mujahith14 <91198292+mujahith14@users.noreply.github.com>
2021-09-22 21:14:26 +0530

    Fundamentals of python programming

---
+++ Module2 Assignment.py
@@ -0,0 +1,39 @@
+# printing multistring content
+mytarget="""I am Mujahith 
+I want to improve my communication Skill
+I want to be a full stack developer also"""
+print(mytarget)
+
+#Finding lenght and count of the particular String
+mytext="India is my country and all Indians are my brothers and sisters"
+print(len(mytext))
+print(mytext.count("my"))
+
+#Performing Arithmetic Operation
+num1=15
+num2=5
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+print(num1%num2)
+
+print(float(num2))
+
+#Print both the Variables in a single print Statement
+sampleText="B.Tech IT"
+sampleNumber=3
+print("I m pursuing "+sampleText+" "+str(sampleNumber)+"rd year in Kalasalingam University")
+
+#Performing Arithmetic Operation with the User Input
+num3=int(input("What is num 3: "))
+num4=int(input("what is num 4: "))
+
+print(num3+num4)
+print(num3-num4)
+print(num3*num4)
+print(num3/num4)
+print(num3%num4)
+
+print(float(num4))
+
